8cbce3e7fa96b48737b5f7dd0dcc22bb2635a239
bluelovers <codelovers@users.sourceforge.net>
2020-06-15 06:59:51 +0800

    feat(awesome): addresses the issue brought up in #133

---
+++ lib/types.d.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by user on 2020/6/15.
+ */
+export declare enum EnumCommitType {
+    feat = "Features",
+    fix = "Bug Fixes",
+    perf = "Performance Improvements",
+    revert = "Reverts",
+    docs = "Documentation",
+    style = "Styles",
+    refactor = "Code Refactoring",
+    test = "Tests",
+    build = "Build System",
+    ci = "Continuous Integration",
+    chore = "Chores"
+}
---
+++ lib/types.ts
@@ -0,0 +1,18 @@
+/**
+ * Created by user on 2020/6/15.
+ */
+
+export enum EnumCommitType
+{
+	feat = 'Features',
+	fix = 'Bug Fixes',
+	perf = 'Performance Improvements',
+	revert = 'Reverts',
+	docs = 'Documentation',
+	style = 'Styles',
+	refactor = 'Code Refactoring',
+	test = 'Tests',
+	build = 'Build System',
+	ci = 'Continuous Integration',
+	chore = 'Chores',
+}
